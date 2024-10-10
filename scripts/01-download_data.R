337177ae35dbe3548f25596ec55114c66e4c2abd
leemarik <mariko.lee@mail.utoronto.ca>
2024-10-10 17:46:22 -0400

    Download president poll data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,33 +1,19 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from Open Data Toronto 
-# Author: Mariko Lee
-# Date: 19 September 2024 
-# Contact: mariko.lee@mail.utoronto.ca
+# Purpose: Downloads and saves the data
+# Authors: Cristina Su Lam, Karen Riani, Mariko Lee
+# Date: 10 October 2024 
 # License: MIT
 # Pre-requisites: None
 # Any other information needed? None
 
-#### Workspace setup ####
-library(opendatatoronto)
-library(tidyverse)
+# Load necessary libraries
+library(readr)
 
-
-#### Download data ####
-# get package
-package <- show_package("e28bc818-43d5-43f7-b5d9-bdfb4eda5feb")
-
-# get all resources for this package
-resources <- list_package_resources("e28bc818-43d5-43f7-b5d9-bdfb4eda5feb")
-
-# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
-datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
-
-# load the first datastore resource as a sample
-data <- filter(datastore_resources, row_number()==1) %>% 
-  get_resource()
-
-
-#### Save data ####
-write_csv(data, "data/raw_data/raw_data.csv") 
-
+# Define the path to the data
+data_path <- "data/raw_data/raw_president_polls.csv"
          
+# Read the CSV file into a data frame
+polls_data <- read_csv(data_path)
+
+# Display the first few rows of the data
+head(polls_data)
