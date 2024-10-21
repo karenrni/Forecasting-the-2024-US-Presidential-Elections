0a2a49ebf4e81fb573d074e1a1c803c26364d3ae
Karen <113319323+karenrni@users.noreply.github.com>
2024-10-21 05:18:36 -0400

    Added initial filters and mutations
    categorized pct, binary for national/state, filtered for Harris and quality

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,21 +1,40 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data
-# Authors: Cristina Su Lam, Karen Riani, Mariko Lee
+# Purpose: Cleans the presidential poll data into an analysis data set
+# Author: Cristina Su Lam, Karen Riani, Mariko Lee
 # Date: 10 October 2024 
 # License: MIT
-# Pre-requisites: None
+# Pre-requisites: Need to have downloaded the data
 # Any other information needed? None
 
-# Load necessary libraries
+#### Workspace setup ####
 library(tidyverse)
-library(readr)
+library(arrow)
 
-# Define the path to the data
-data_path <- "data/raw_data/raw_president_polls.csv"
-         
-# Read the CSV file into a data frame
-polls_data <- read_csv(data_path)
+#### Clean data ####
+polls_data <- read_csv("data/raw_data/raw_president_polls.csv")
 
-# Display the first few rows of the data
-head(polls_data)
+#polls_data <- polls_data %>%
+ # janitor::clean_names()
+
+# Handle missing values (example: remove incomplete rows)
+#polls_data <- polls_data %>%
+ # drop_na()
+
+# Binary for state or national polls
+polls_data <- polls_data %>%
+  mutate(is_state = as.numeric(!is.na(state)))
+
+# Categorize pct for Harris into High and Low Support
+polls_data <- polls_data %>%
+  mutate(pct = as.factor(ifelse(pct > 50, "High", "Low")))
+
+# Filter for high quality pollsters and state-specific Harris polls
+polls_data %>%
+  filter(numeric_grade >= 3.0, 
+         candidate_name == "Kamala Harris")
+
+#### Save data ####
+
+write_parquet(polls_data, "data/analysis_data/polls_data.parquet")
+# delete analysis data csv 
 
