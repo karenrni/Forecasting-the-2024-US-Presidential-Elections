dc961aa637108bdbb8a9ab4e9a13a753c1f5f754
Karen <113319323+karenrni@users.noreply.github.com>
2024-10-21 05:15:47 -0400

    Added inital simulation- training and test sets + logistic regression
    issue with my RStudio, had to manually add

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -8,14 +8,49 @@
 
 #### Workspace setup ####
 library(tidyverse)
+library(tidymodels)
+library(arrow)
 
+#### Read Data ####
+
+upef2024_data <- 
+  read_parquet(file = here::here("data/analysis_data/polls_data.parquet"))
+# here::here not necessary, just more robust 
 
 #### Simulate data ####
-set.seed(304)
+set.seed(333)
 
-# Define the start and end date
-start_date <- as.Date("2018-01-01")
-end_date <- as.Date("2023-12-31")
+## Split data into test and training sets
+upef2024_split <- initial_split(upef2024_data, prop = 0.80)
+upef2024_train <- training(upef2024_split)
+upef2024_test <- testing(upef2024_split)
+
+## Fit logistic model
+upef2024_tidymodels <-
+  logistic_reg(mode = "classification") |>
+  set_engine("glm") |>
+  fit(
+    pct ~ pollster + is_state,
+    data = upef2024_train
+  )
+
+upef2024_tidymodels
+
+## Evaluate on test set.. this is not
+upef2024_tidymodels |>
+  predict(new_data = upef2024_test) |>
+  cbind(upef2024_test) |>
+  conf_mat(truth = pct, estimate = .pred_class)
+
+## K-Fold Cross Validation
+
+
+
+### Simulation Tests
+
+# Confirm the start and end date
+start_date <- as.Date("2024-01-01")
+end_date <- as.Date("2024-00-00")
 
 # Set the number of random dates you want to generate
 number_of_dates <- 100
