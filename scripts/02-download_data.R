d2808042d30d8879e82621954768842d29ec7ae0
Cristina Su Lam <cristina.sulam@mail.utoronto.ca>
2024-10-22 14:01:25 -0400

    Read and Cleaned Raw Data

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -1,26 +1,16 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Download the presidential poll data 
+# Author: Cristina Su Lam, Karen Riani, Mariko Lee
+# Date: 10 October 2024 
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Pre-requisites: None
+# Any other information needed? None
 
 #### Workspace setup ####
-library(opendatatoronto)
-library(tidyverse)
-# [...UPDATE THIS...]
+library(readr)
 
-#### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+#### Download/Read Dataset ####
+raw_president_polls <- read_csv("data/01-raw_data/raw_president_polls.csv")
 
-
-
-#### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
 
          
--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -1,44 +1,66 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Cleans the presidential poll data into an analysis data set
+# Author: Cristina Su Lam, Karen Riani, Mariko Lee
+# Date: 10 October 2024 
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Run 01-download_data.R
+# Any other information needed? None
 
 #### Workspace setup ####
+library(readr)
 library(tidyverse)
+library(arrow)
+library(janitor)
+library(lubridate)
+library(broom)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+clean_president_polls <- read_csv("data/01-raw_data/raw_president_polls.csv", 
+                                  col_types = cols(poll_id = col_skip(), 
+                                                   pollster_id = col_skip(), sponsor_ids = col_skip(), 
+                                                   sponsors = col_skip(), display_name = col_skip(), 
+                                                   pollster_rating_id = col_skip(), 
+                                                   pollster_rating_name = col_skip(), 
+                                                   pollscore = col_skip(), methodology = col_skip(), 
+                                                   transparency_score = col_skip(), 
+                                                   sponsor_candidate_id = col_skip(), 
+                                                   sponsor_candidate = col_skip(), sponsor_candidate_party = col_skip(), 
+                                                   endorsed_candidate_id = col_skip(), 
+                                                   endorsed_candidate_name = col_skip(), 
+                                                   endorsed_candidate_party = col_skip(), 
+                                                   question_id = col_skip(), population = col_skip(), 
+                                                   subpopulation = col_skip(), population_full = col_skip(), 
+                                                   tracking = col_skip(), created_at = col_skip(), 
+                                                   notes = col_skip(), url = col_skip(), 
+                                                   url_article = col_skip(), url_topline = col_skip(), 
+                                                   url_crosstab = col_skip(), source = col_skip(), 
+                                                   internal = col_skip(), partisan = col_skip(), 
+                                                   race_id = col_skip(), cycle = col_skip(), 
+                                                   office_type = col_skip(), seat_number = col_skip(), 
+                                                   seat_name = col_skip(), election_date = col_skip(), 
+                                                   stage = col_skip(), nationwide_batch = col_skip(), 
+                                                   ranked_choice_reallocated = col_skip(), 
+                                                   ranked_choice_round = col_skip(), 
+                                                   hypothetical = col_skip(), party = col_skip(), 
+                                                   answer = col_skip(), candidate_id = col_skip())) |>
+  clean_names()
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
+# Create binary variable for state or national polls
+clean_president_polls <- clean_president_polls %>%
   mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+    state = str_replace(state, "Maine CD-[12]", "Maine"),
+    state = str_replace(state, "Nebraska CD-2", "Nebraska"),
+    is_state = as.numeric(!is.na(state)),
+    end_date = mdy(end_date),
+    start_date = mdy(start_date),
+    is_harris = ifelse(candidate_name == "Kamala Harris", 1, 0))
+
+
+# Filter for high quality pollsters and state-specific Harris and Trump polls
+clean_president_polls <- clean_president_polls %>%
+  filter(
+    candidate_name %in% c("Kamala Harris", "Donald Trump")
+  ) 
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_parquet(clean_president_polls, "data/02-analysis_data/clean_president_polls.parquet")
