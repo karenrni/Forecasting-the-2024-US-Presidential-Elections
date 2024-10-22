b56dac89bb99d65f37960efcdc5c079f7343ac48
Cristina Su Lam <cristina.sulam@mail.utoronto.ca>
2024-10-22 12:32:14 -0400

    Cleaned Raw Dataset

--- scripts/01_data_cleaning_and_preparation/02-data_cleaning.R
+++ scripts/01_data_cleaning_and_preparation/02-data_cleaning.R
@@ -3,10 +3,11 @@
 # Author: Cristina Su Lam, Karen Riani, Mariko Lee
 # Date: 10 October 2024 
 # License: MIT
-# Pre-requisites: Need to have downloaded the data
+# Pre-requisites: Run 01-download_data.R
 # Any other information needed? None
 
 #### Workspace setup ####
+library(readr)
 library(tidyverse)
 library(arrow)
 library(janitor)
@@ -14,33 +15,52 @@ library(lubridate)
 library(broom)
 
 #### Clean data ####
-high_quality_harris <- read_csv("data/raw_data/raw_president_polls.csv") |>
+clean_president_polls <- read_csv("data/raw_data/raw_president_polls.csv", 
+                                col_types = cols(poll_id = col_skip(), 
+                                                 pollster_id = col_skip(), sponsor_ids = col_skip(), 
+                                                 sponsors = col_skip(), display_name = col_skip(), 
+                                                 pollster_rating_id = col_skip(), 
+                                                 pollster_rating_name = col_skip(), 
+                                                 pollscore = col_skip(), methodology = col_skip(), 
+                                                 transparency_score = col_skip(), 
+                                                 sponsor_candidate_id = col_skip(), 
+                                                 sponsor_candidate = col_skip(), sponsor_candidate_party = col_skip(), 
+                                                 endorsed_candidate_id = col_skip(), 
+                                                 endorsed_candidate_name = col_skip(), 
+                                                 endorsed_candidate_party = col_skip(), 
+                                                 question_id = col_skip(), population = col_skip(), 
+                                                 subpopulation = col_skip(), population_full = col_skip(), 
+                                                 tracking = col_skip(), created_at = col_skip(), 
+                                                 notes = col_skip(), url = col_skip(), 
+                                                 url_article = col_skip(), url_topline = col_skip(), 
+                                                 url_crosstab = col_skip(), source = col_skip(), 
+                                                 internal = col_skip(), partisan = col_skip(), 
+                                                 race_id = col_skip(), cycle = col_skip(), 
+                                                 office_type = col_skip(), seat_number = col_skip(), 
+                                                 seat_name = col_skip(), election_date = col_skip(), 
+                                                 stage = col_skip(), nationwide_batch = col_skip(), 
+                                                 ranked_choice_reallocated = col_skip(), 
+                                                 ranked_choice_round = col_skip(), 
+                                                 hypothetical = col_skip(), party = col_skip(), 
+                                                 answer = col_skip(), candidate_id = col_skip())) |>
   clean_names()
 
-# Binary for state or national polls
-high_quality_harris <- high_quality_harris %>%
-  mutate(is_state = as.numeric(!is.na(state)),
-  end_date = mdy(end_date))
-
-# Categorize pct for Harris into High and Low Support in new col
-high_quality_harris$pct_category <- ifelse(high_quality_harris$pct > 50, "High", "Low")
-high_quality_harris$pct_category <- as.factor(high_quality_harris$pct_category)
-
-# Filter for high quality pollsters and state-specific Harris polls
-high_quality_harris %>%
-  filter(
-    numeric_grade >= 3.0, 
-    candidate_name == "Kamala Harris"
-  ) 
-
-high_quality_harris <- high_quality_harris %>%
-  filter(end_date >= as.Date("2024-07-21"))
-high_quality_harris <- high_quality_harris %>%
+# Create binary variable for state or national polls
+clean_president_polls <- clean_president_polls %>%
   mutate(
-    num_harris = round((pct / 100) * sample_size, 0) # Convert pct to actual count of Harris supporters
-  )
+    is_state = as.numeric(!is.na(state)),
+    end_date = mdy(end_date),
+    start_date = mdy(start_date),
+    is_harris = ifelse(candidate_name == "Kamala Harris", 1, 0))
+
+# Filter for high quality pollsters and state-specific Harris and Trump polls
+clean_president_polls <- clean_president_polls %>%
+  filter(numeric_grade >= 3.0,
+         candidate_name %in% c("Kamala Harris", "Donald Trump"),
+         start_date >= as.Date("2024-07-21")
+         ) 
 
 #### Save data ####
-write_parquet(high_quality_harris, "data/analysis_data/high_quality_harris.parquet")
+write_parquet(clean_president_polls, "data/analysis_data/clean_president_polls.parquet")
 
-# delete analysis data csv 
+
