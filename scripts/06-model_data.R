c090dc19425b5892b877deab53decd7abf34ebb7
Cristina Su Lam <cristina.sulam@mail.utoronto.ca>
2024-10-25 12:43:17 -0400

    Added Model Validation for 1st Model

--- scripts/06-model_data.R
+++ scripts/06-model_data.R
@@ -12,13 +12,18 @@ library(tidyverse)
 library(arrow)
 library(dplyr)
 library(rstanarm)
+library(pROC)
 
 #### Read upcoming presidential election forecast data ####
 clean_president_polls <- read_parquet("data/02-analysis_data/clean_president_polls.parquet")
 
+# Convert categorical variables to factors
+clean_president_polls$pollster <- as.factor(clean_president_polls$pollster)
+clean_president_polls$state <- as.factor(clean_president_polls$state)
+
 ### Logistic Regression ###
 
-# Normalize weights to a reasonable scale
+# Weights
 clean_president_polls <- clean_president_polls %>%
   mutate(weight = sample_size / mean(sample_size)) %>%
   mutate(weight = weight * numeric_grade)
@@ -34,8 +39,45 @@ model_logistic
 # Predict the probabilities of Harris winning
 clean_president_polls <- clean_president_polls %>%
   mutate(predicted_prob_harris = predict(model_logistic, type = "response"))
-clean_president_polls
 
+
+# Model Validation & Checking
+set.seed(123)
+train_indices <- sample(seq_len(nrow(clean_president_polls)), size = 0.7 * nrow(clean_president_polls))
+
+# Create training and test datasets
+train_data <- clean_president_polls[train_indices, ]
+test_data <- clean_president_polls[-train_indices, ]
+
+model_logistic_train <- glm(
+  is_harris ~ pollster + state + sample_size + pct + pollster:pct,
+  data = train_data,
+  family = binomial(link = "logit"),
+  weights = weight
+)
+
+
+# Remove unused levels from the model object
+model_logistic_clean <- model_logistic
+model_logistic_clean$xlevels$pollster <- intersect(levels(test_data$pollster), levels(train_data$pollster))
+model_logistic_clean$xlevels$state <- intersect(levels(test_data$state), levels(train_data$state))
+
+# Run the prediction
+test_data <- test_data %>%
+  mutate(predicted_prob_harris = predict(model_logistic_clean, newdata = test_data, type = "response"))
+
+# Create a binary prediction (Harris wins if probability > 0.5)
+test_data <- test_data %>%
+  mutate(prediction = ifelse(predicted_prob_harris > 0.5, 1, 0))
+
+# Calculate accuracy
+accuracy <- mean(test_data$prediction == test_data$is_harris)
+print(accuracy)
+
+# Calculate AUC
+roc_curve <- roc(test_data$is_harris, test_data$predicted_prob_harris)
+auc_value <- auc(roc_curve)
+print(auc_value)
 
 ### Bayesian Model ###
 
